dbe52af742a3174cad61df8370264356de59b841
Luan Novais <luaannovais@live.com>
2021-02-22 18:22:14 -0300

    Feat: experience bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { Button } from './components/Button'
+import { ExperienceBar } from './components/ExperienceBar';
+
+import './styles/global.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello NLW</h1>
-      <Button
-        color="red"
-      >
-        Button 1
-      </Button>
-      <Button
-        color="blue"
-      >
-        Button 2
-      </Button>
-      <Button
-        color="green"
-      >
-        Button 3
-      </Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
